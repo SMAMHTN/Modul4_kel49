23e6c7918ef1fafe5a0e3d9417ed201ee831ec32
Pratama Wibi <uchihapratama135@gmail.com>
2020-04-17 14:53:50 +0700

    Update Main Wibi
    gas

--- Tugas Modul 4 Kelompok 49/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ Tugas Modul 4 Kelompok 49/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -3,6 +3,7 @@ import Ext_Source_Code
 def creator() :
     print("Program ini dibuat oleh kelompok 49 Shift 2")
     print("Aldi Mulyawan 21120119120026")
+    print("Mochamad Pratama Wibawa 21120119130062")
 
 print("Selamat datang di Mesin Toko Emas Otomatis Kelompok 49!")
 nama=str(input("Masukkan Nama anda\t:"))
@@ -37,3 +38,27 @@ while (choice==1):
             print("Detail transaksi akan dikirimkan ke email ",email," serta no HP ",HP)
         print("----------------------------------------------------------------")
 
+    elif (a==2):
+        call.pembelian_fitur()
+        b=int(input("Masukkan nomor logam yang ingin anda beli\t\t: "))
+        c=int(input("Masukkan berat logam yang akan anda beli (Dalam Gram)\t:"))
+        call.pembelian_transaksi(b, c)
+        e=int(input("Masukkan Jawaban anda : "))
+        if (e==1):
+            print("Terima Kasih telah bertransaksi menggunakan Mesin Toko Emas Otomatis Kelompok 49!")
+            print("Detail transaksi akan dikirimkan ke email ",email," serta no HP ",HP)
+        print("----------------------------------------------------------------")
+    elif (a==3):
+        call.about_fitur()
+    elif (a==4):
+        creator()
+    else:
+        print("Mohon masukkan angka yang sesuai!")
+
+    print("Apakah anda ingin melakukan transaksi lainnya?")
+    print("1.Ya")
+    print("2.Tidak")
+    choice=int(input("Masukkan pilihan anda : "))
+    print("----------------------------------------------------------------")
+
+print("Terima Kasih telah menggunakan Mesin Toko Emas Otomatis Kelompok 49!")

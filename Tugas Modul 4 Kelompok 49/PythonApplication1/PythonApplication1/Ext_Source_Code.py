27635d838ab2b15cb45512f8da96cfa8be88bb6b
SMAMHTN <aldismartkid@gmail.com>
2020-04-18 21:56:34 +0700

    Formatting
    AldiM
    Added formatting

--- Tugas Modul 4 Kelompok 49/PythonApplication1/PythonApplication1/Ext_Source_Code.py
+++ Tugas Modul 4 Kelompok 49/PythonApplication1/PythonApplication1/Ext_Source_Code.py
@@ -1,10 +1,10 @@
 class main:
     def __init__ (self, nama, email, HP):
-        self.nama=nama
-        self.email=email
-        self.HP=HP
-        self.logam=["Emas","Logam"]
-        self.harga=[732600,216044]
+        self.nama = nama
+        self.email = email
+        self.HP = HP
+        self.logam = ["Emas","Logam"]
+        self.harga = [732600,216044]
 
     def welcome (self):
         print(f"Selamat datang, {self.nama} ! Selamat bertransaksi di Mesin Toko Emas Otomatis Kelompok 49!")
@@ -12,37 +12,37 @@ class main:
         print("Penjualan\t:5%")
         print("Pembelian\t:10%")
 
-    def pembelian_fitur(self):
+    def pembelian_fitur (self):
         print("Masukkan jenis logam yang ingin anda beli")
-        count=1
+        count = 1
         for x in self.logam :
-            print (count,".",x)
-            count=count+1
+            print(count,".",x)
+            count = count + 1
 
-    def penjualan_fitur(self):
+    def penjualan_fitur (self):
         print("Masukkan jenis logam yang ingin anda jual")
-        count=1
+        count = 1
         for x in self.logam :
-            print (count,".",x)
-            count=count+1
+            print(count,".",x)
+            count = count + 1
 
-    def about_fitur(self):
+    def about_fitur (self):
         print("Berikut adalah Value Logam pada hari ini (per gram):")
-        count=1
+        count = 1
         for x in self.logam :
-            print (count,".",x,"\t: Rp ",self.harga[count-1])
-            count=count+1
+            print(count,".",x,"\t: Rp ",self.harga[count - 1])
+            count = count + 1
 
-    def pembelian_transaksi(self,angka1,angka2):
-        biaya=self.harga[angka1-1]*angka2*105/100
+    def pembelian_transaksi (self,angka1,angka2):
+        biaya = self.harga[angka1 - 1] * angka2 * 105 / 100
         print(f"Anda ingin membeli logam {self.logam[angka1-1]} dengan harga {biaya}")
         print("----------------------------------------------------------------")
         print("Lanjutkan Pembelian?")
         print("1.Ya")
         print("2.Tidak")
 
-    def penjualan_transaksi(self,angka1,angka2):
-        biaya=self.harga[angka1-1]*angka2*90/100
+    def penjualan_transaksi (self,angka1,angka2):
+        biaya = self.harga[angka1 - 1] * angka2 * 90 / 100
         print(f"Anda ingin menjual logam {self.logam[angka1-1]} dengan harga {biaya}")
         print("----------------------------------------------------------------")
         print("Lanjutkan Pembelian?")
